56b4bed41cb7b2711a7fdbcc27107a49144540bc
Luiz047 <luizjr@live.com>
2023-02-22 12:37:37 -0300

    button

--- src/Componentes/Serviços/index.js
+++ src/Componentes/Serviços/index.js
@@ -14,7 +14,7 @@ function Serviços() {
                         <p className="title">Corrente Prata Italiana</p>
                         <p> Uma excelente escolha para quem busca um acessório de prata de alta qualidade, durável e elegante.
                         </p>
-                        <button> VER </button>
+                        <button><a href='https://api.whatsapp.com/message/OAXTQ5Q72AJZM1?autoload=1&app_absent=0' target="_blank" >Faça seu pedido </a> </button>
                         </div>
                         </div>
                       <div className="card">
@@ -22,7 +22,7 @@ function Serviços() {
                         <div class="container">
                         <p className="title">Corrente folheada a Prata Italiana</p>
                         <p>A prata italiana folheada adiciona um brilho discreto à peça e a alta qualidade do material garante durabilidade e resistência.</p>
-                        <button> VER </button>
+                        <button><a href='https://api.whatsapp.com/message/OAXTQ5Q72AJZM1?autoload=1&app_absent=0' target="_blank" >Faça seu pedido </a> </button>
                         </div>
                         </div>
                       <div className="card">
@@ -31,7 +31,7 @@ function Serviços() {
                         <p className="title">Anel Versace Premium</p>
                         <p>Este acessório é perfeito para uso em eventos especiais ou para adicionar um toque de estilo ao seu visual diário.
                         </p>
-                        <button> VER </button>
+                        <button><a href='https://api.whatsapp.com/message/OAXTQ5Q72AJZM1?autoload=1&app_absent=0' target="_blank" >Faça seu pedido </a> </button>
                         </div>
                         </div>
                       <div className="card">
@@ -40,7 +40,7 @@ function Serviços() {
                         <p className="title">Corrente folheada a Prata Italiana</p>
                         <p>Uma peça de moda elegante e sofisticada, com um design clássico e atemporal.
                         </p>
-                        <button> VER </button>
+                        <button><a href='https://api.whatsapp.com/message/OAXTQ5Q72AJZM1?autoload=1&app_absent=0' target="_blank" >Faça seu pedido </a> </button>
                         </div>
                         </div>
 
@@ -50,7 +50,7 @@ function Serviços() {
                         <p className="title" >Correntes de ouro 18K</p>
                         <p>Essas correntes vêm em diversos designs, espessuras e comprimentos para atender a diferentes gostos e estilos.
                         </p>
-                        <button> VER </button>
+                        <button><a href='https://api.whatsapp.com/message/OAXTQ5Q72AJZM1?autoload=1&app_absent=0' target="_blank" >Faça seu pedido </a> </button>
                         </div>
                         </div>
                       <div className="card">
@@ -59,16 +59,16 @@ function Serviços() {
                         <p className="title">Corrente Figaro 3x1 Prata Italiana 60cm e 6mm</p>
                         <p>Com um design clássico, a corrente apresenta um padrão de elos 3x1, onde tres elos pequenos são intercalados por um elo maior, resultando em um estilo único e atraente.
                         </p>
-                        <button> VER </button>
+                        <button><a href='https://api.whatsapp.com/message/OAXTQ5Q72AJZM1?autoload=1&app_absent=0' target="_blank" >Faça seu pedido </a> </button>
                         </div>
                         </div>
                       <div className="card">
-                        <img src="./assets/Pulseira.jpg"alt="Produto"/>
+                        <img src="./assets/Pulseira.jpg" alt="Produto"/>
                         <div class="container">
                         <p className="title" >Pulseira de Prata Italiana 20cm </p>
                         <p>A pulseira de Prata Italiana apresenta um design clássico, tornando-a uma opção atemporal e elegante para diversas ocasiões.
                         </p>
-                        <button> VER </button>
+                        <button><a href='https://api.whatsapp.com/message/OAXTQ5Q72AJZM1?autoload=1&app_absent=0' target="_blank" >Faça seu pedido </a> </button>
                         </div>
                         </div>
                       <div className="card">
@@ -77,7 +77,7 @@ function Serviços() {
                         <p className="title" >Corrente veneziana de prata Italiana 60cm </p>
                         <p>Uma corrente veneziana de prata italiana pode durar por muitos anos, tornando-se uma peça clássica e valiosa em sua coleção de joias.
                         </p>
-                        <button> VER </button>
+                        <button><a href='https://api.whatsapp.com/message/OAXTQ5Q72AJZM1?autoload=1&app_absent=0' target="_blank" >Faça seu pedido </a> </button>
                         </div>
                     </div>
                 
